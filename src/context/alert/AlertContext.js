5f21010813e3c8cb9ffd0eb2ec0c5ee34b963c55
TomasGarciaDev <tomasgarciadev@gmail.com>
2024-10-15 18:34:12 +0200

    Alert Component

--- src/context/alert/AlertContext.js
+++ src/context/alert/AlertContext.js
@@ -1,15 +1,14 @@
 import { createContext, useReducer } from "react";
-import alertReducer from "./AlertContext";
+import AlertReducer from "./AlertReducer";
 
 const AlertContext = createContext();
 
 export const AlertProvider = ({ children }) => {
   const initialState = null;
 
-  const [state, dispatch] = useReducer(alertReducer, initialState);
+  const [state, dispatch] = useReducer(AlertReducer, initialState);
 
   // Set an alert
-
   const setAlert = (msg, type) => {
     dispatch({
       type: "SET_ALERT",
